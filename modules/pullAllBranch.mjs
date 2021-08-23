1b8f82c41edfcbfa5dd9632cc23acfb0e591df3f
zhaorui <zhaorui27@sfmail.sf-express.com>
2021-08-23 19:42:54 +0800

    fix: 修复 git merge 命令使用方法不对

--- modules/pullAllBranch.mjs
+++ modules/pullAllBranch.mjs
@@ -13,17 +13,22 @@ const pullAllBranch = async () => {
   cd(config.directory);
   let remoteBranch = await getAllRemoteBranch({ remoteName });
   runAsync(
-    ...remoteBranch.map((item) => {
-      return {
-        cmd: `git merge ${remoteName}/${item} ${item}`,
+    ...remoteBranch.reduce((result, item) => {
+      result[result.length] = {
+        cmd: `git switch ${item}`,
         handleError: ({ stderr }, res) => {
           // 本地分支不存在
-          if (/merge: \S+ - not something we can merge/.test(stderr)) {
-            $`git checkout -t ${remoteName}/${item}`.then(res);
+          if (/fatal: invalid reference: \S+/.test(stderr)) {
+            runAsync(
+              `git checkout -t ${remoteName}/${item}`,
+              `git switch ${item}`
+            ).then(res);
           }
         },
       };
-    })
+      result[result.length] = `git merge ${remoteName}/${item}`;
+      return result;
+    }, [])
   );
 };
 export default pullAllBranch;
